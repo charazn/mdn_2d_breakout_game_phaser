40aa8839a1d3a84e799838c652ac6310e17b9642
Char Aznable <charazn37@gmail.com>
2016-12-12 10:49:25 +0800

    Add phaser scale object

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
   update: update
 });
 
-function preload() {}
+function preload() {
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  // SHOW_ALL — scales the canvas, but keeps the aspect ratio untouched, so images won't be skewed like in the previous mode. There might be black stripes visible on the edges of the screen, but we can live with that.
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  // These two lines of code in the preload() function are responsible for aligning the canvas element horizontally and vertically, so it is always centered on screen regardless of size.
+}
 
 function create() {}
 
