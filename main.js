183d755457c72fe0881953ad8f40b033fe2173ec
Char Aznable <charazn37@gmail.com>
2016-12-12 22:25:56 +0800

    Add ballLeaveScreen function to refresh screen show lives left

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function preload() {
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.arcade.checkCollision.down = false;
-  ball = game.add.sprite(game.world.width * 0.5, game.world.height - 50, 'ball'); // From ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(game.world.width * 0.5, game.world.height - 50, 'ball'); // From ball = game.add.sprite(50, 50, 'ball'); and from -25
   ball.anchor.set(0.5);
   game.physics.enable(ball, Phaser.Physics.ARCADE);
 
@@ -39,10 +39,8 @@ function create() {
   ball.body.collideWorldBounds = true; // Tell the framework that we want to treat the boundaries of the <canvas> element as walls and not let the ball move past them.
   ball.body.bounce.set(1); // To make ball baounce off wall, we have to set its bounciness.
   ball.checkWorldBounds = true; // This will make the ball check the world bounds 
-  ball.events.onOutOfBounds.add(function () { // and execute the function bound to the onOutOfBounds event
-    alert('Game over!');
-    location.reload();
-  }, this);
+  // ball.events.onOutOfBounds.add(function () { alert('Game over!'); location.reload(); }, this); // and execute the function bound to the onOutOfBounds event
+  ball.events.onOutOfBounds.add(ballLeaveScreen, this); // The add() method binds the given function and causes it to be executed every time the event occurs
 
   paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
   paddle.anchor.set(0.5, 1); // Mid-width of paddle, bottom of paddle
@@ -51,7 +49,7 @@ function create() {
 
   initBricks();
 
-  textStyle = { font: '18px Arial', fill: '#0095DD' };  
+  textStyle = { font: '18px Arial', fill: '#0095DD' };
   scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
 
   livesText = game.add.text(game.world.width - 5, 5, 'Lives: ' + lives, textStyle); // Set at 5px from top right corner 
@@ -112,6 +110,23 @@ function ballHitBrick(ball, brick) { // How does the engine know which brick the
   }
   if (count_alive === 0) {
     alert('You won the game, congratulations!');
+    location.reload();
+  }
+}
+
+function ballLeaveScreen() {
+  lives--;
+  if (lives) {
+    livesText.setText('Lives: ' + lives);
+    lifeLostText.visible = true;
+    ball.reset(game.world.width * 0.5, game.world.height - 50); // From 25
+    paddle.reset(game.world.width * 0.5, game.world.height - 5);
+    game.input.onDown.addOnce(function () { // addOnce() is useful when you want to have the bound function executed only once and then unbound so it is not executed again
+      lifeLostText.visible = false;
+      ball.body.velocity.set(150, -150);
+    }, this);
+  } else {
+    alert('You lost, game over!');
     location.reload();
   }
 }
