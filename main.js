e623c160715b49ba4ff2eb1599134706c5d158b7
Char Aznable <charazn37@gmail.com>
2016-12-12 15:03:30 +0800

    Set ball position in middle of width and velocity to go upwards

--- main.js
+++ main.js
@@ -20,13 +20,15 @@ function preload() {
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  ball = game.add.sprite(50, 50, 'ball');
+  // ball = game.add.sprite(50, 50, 'ball');
+  ball = game.add.sprite(game.world.width * 0.5, game.world.height - 50, 'ball');
+  ball.anchor.set(0.5);
   game.physics.enable(ball, Phaser.Physics.ARCADE);
   ball.body.collideWorldBounds = true;
   // Tell the framework that we want to treat the boundaries of the <canvas> element as walls and not let the ball move past them.
   ball.body.bounce.set(1);
   // To make ball baounce off wall, we have to set its bounciness.
-  ball.body.velocity.set(150, 150);
+  ball.body.velocity.set(150, -150); // From (150, 150)
 
   paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
   paddle.anchor.set(0.5, 1); // Mid-width of paddle, bottom of paddle
